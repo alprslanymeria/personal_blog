bd548496b266a6ce24414d930d3ad77d59c1a878
alparslan <alparslanorgnl@gmail.com>
2025-02-22 20:17:40 +0300

    ikonlara color prop eklendi

--- src/components/LinkComponent.tsx
+++ src/components/LinkComponent.tsx
@@ -12,13 +12,13 @@ export default function LinkComponent() {
 
         <div className="flex justify-center md:justify-start space-x-4">
           <Link href="https://www.linkedin.com/in/idrisalparslan/" target="_blank" className="text-blue-600 hover:text-blue-800">
-            <LinkedinIcon/>
+            <LinkedinIcon color="blue"/>
           </Link>
           <Link href="https://twitter.com" target="_blank" className="text-blue-400 hover:text-blue-600">
-            <TwitterIcon/>
+            <TwitterIcon color="black"/>
           </Link>
           <Link href="https://github.com/alprslanymeria" target="_blank" className="text-pink-500 hover:text-pink-700">
-            <GithubIcon/>
+            <GithubIcon color="pink"/>
           </Link>
         </div>
     )
