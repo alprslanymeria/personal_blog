b5494f3af348152d1db7881a16e30806a39b7e79
alparslan <alparslanorgnl@gmail.com>
2025-02-22 22:33:00 +0300

    domains to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['storage.googleapis.com']
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'storage.googleapis.com'
+    }]
   }
 };
 
