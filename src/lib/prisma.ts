895c7abb2d0e175281ee56fdea1ac40f52c25c42
alparslan <alparslanorgnl@gmail.com>
2025-10-23 22:29:44 +0300

    prisma.js file changed toprisma. ts file

---
+++ src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client"
+
+declare global {
+  
+  var prisma: PrismaClient | undefined
+}
+
+export const prisma = globalThis.prisma || new PrismaClient()
+
+globalThis.prisma = prisma
